95526ae1ceebec84a757d7f0b90ed348468929df
Yazan Abu Sneneh <33704283+YazanSneneh@users.noreply.github.com>
2022-04-22 21:08:23 +0300

    Delete blogapp/core directory

--- blogapp/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.homepage, name='homepage'),
-    path('signup/', views.signup, name='signup'),
-    path('profile/',views.profile, name='profile'),
-]
